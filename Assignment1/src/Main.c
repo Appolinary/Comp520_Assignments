a374bc1dca2bcd4add488bd784d1f45ce09f08b8
Fabrice Appolinary <fabricyappolinary@gmail.com>
2019-01-23 16:27:54 -0500

    Update Main.c

--- Assignment1/src/Main.c
+++ Assignment1/src/Main.c
@@ -1,6 +1,39 @@
-void yyparse();
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
-	yyparse();
+void yyparse();
+int yylex();
+
+int print_token = 0;
+
+int main(int argc, char ** argv){
+
+	if(argc != 3){
+		printf("Enter the right number of arguments \n");
+		return 0;
+	}
+
+	char * mode = argv[1];
+	char * tokens = "tokens";
+	char * scan = "scan";
+	char * parse = "parse";
+
+	print_token = 0;
+
+	if(strcmp(mode, scan) == 0){
+		while(yylex());
+		printf("OK\n");
+	}
+
+	if(strcmp(mode, tokens) == 0){
+		print_token =  1;
+		while(yylex());
+	}
+	if(strcmp(mode, parse) == 0){
+		yyparse();
+	}
+
+
 	return 0;
 }
